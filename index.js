ce9825b4136409a1eac3edb52c58c4bdb26c7dce
gaurabwebdev <gaurabbose40@gmail.com>
2023-05-02 19:29:42 +0600

    json file merged

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
+const chefs = require("./data/chefs.json");
+const recipeDetails = require("./data/recipes.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Chef");
+});
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+app.get("/chefs/:id", (req, res) => {
+  const chefId = req.params.id;
+  const currentChef = chefs.find((recipe) => recipe.id === parseInt(chefId));
+  res.send(currentChef);
+  // res.send(recipeDetails);
 });
 
 app.listen(port, () => {
